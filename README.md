c3b96669a09126bb280f061a014c7461706b0e85
Salomao S <souza95salomao@gmail.com>
2022-08-17 14:12:54 +0000

    update: day 02 finished

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 A challenge to code w1 days straight.
 
 - [x] Day 01: Create repository for challenges.
+- [x] Day 02: An awesome hello world.
+
+> Current streak: 02/21 and counting.. :D
---
+++ day_02/src/components/HelloWorld.vue
@@ -0,0 +1,43 @@
+<script setup>
+defineProps({
+  msg: String
+})
+</script>
+
+<template>
+<p>Felizes os devs que não temem a maldição do HelloWorld...</p>
+<h1>
+  {{ msg.split(' ')[0] }}
+	&nbsp;•&nbsp;
+	<span>{{ msg.split(' ')[1] }}</span>
+</h1>
+</template>
+
+<style scoped>
+h1 {
+  color: #eee;
+	font-size: 3rem;
+}
+
+span {
+  color: #188678;
+	font-size: 3rem;
+}
+
+p {
+  max-width: 80vw;
+	margin-inline: auto;
+	margin-bottom: 20vh;
+}
+
+@media(min-width:900px) {
+  p {
+	  font-size: 3rem;
+		line-height: 1.3;
+	}
+
+  span, h1 {
+	  font-size: 8rem;
+	}
+}
+</style>
